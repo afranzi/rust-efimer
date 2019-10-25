64feaeb078ecc16eb11476988e498abeca4f6363
Albert Franzi <franzicros@gmail.com>
2019-10-25 14:09:08 +0200

    :wrench: - get_hrefs workings

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,27 @@ fn get_body(url: &str) -> Result<String, Error> {
     reqwest::get(url)?.text()
 }
 
+fn get_hrefs(doc: Document) -> Vec<Option<String>> {
+    doc.find(Attr("id", "issues-list").descendant(Class("issue-title")).descendant(Name("a")))
+        .map(|x| x.attr("href").map(|href| String::from(href)))
+        .collect::<Vec<_>>()
+}
+
 fn grab_issues(issues_url: &str) {
     let resp = get_body(issues_url);
     let document = resp.map(|body| Document::from(&*body));
+    let issues: Result<Vec<Option<String>>, Error> = document.map(|doc| get_hrefs(doc));
 
-    let issues: Vec<Option<String>> = document.into_iter().map(|doc|
-        doc.find(Attr("id", "issues-list").descendant(Class("issue-title")).descendant(Name("a")))
-            .map(|x| x.attr("href"))
-            .collect::<Vec<_>>()
-    );
+    issues.map(|x| {
+        println!("Length {}", x.len());
 
-    issues.map(|x| x.into_iter().map(|issue| println!("{}", issue.unwrap())));
+        for issue in &x {
+            match issue {
+                Some(du) => println!("{}", du),
+                None => println!("Empty"),
+            }
+        }
+    });
 }
 
 fn main() {
